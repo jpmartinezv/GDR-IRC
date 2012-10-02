afebaee75c8cf9c1fd264029d2474876c3e39ca2
Jason Martínez <jason1994.8@gmail.com>
2012-10-02 12:57:10 -0500

    Commands 65%

--- src/lib/cmds.py
+++ src/lib/cmds.py
@@ -55,15 +55,40 @@ class Cmds:
             content = auto_comas(channels)
         else:
             content = auto_comas(channels) + " :" + message
-        return toBytes("PART" + content + "\r\n")
+        return toBytes("PART " + content + "\r\n")
     
     def Topic(self, channel, topic=None):
         if(topic is None):
             content = channel
         else:
             content = channel + " :" + topic
-        return toBytes("PART" + content + "\r\n")
+        return toBytes("TOPIC " + content + "\r\n")
             
-            
+    def Names(self, channels=None):
+        if(channels is None):
+            content = " "
+        else:
+            content = auto_comas(channels)
+        return toBytes("NAMES " + content + "\r\n")
+
+    def Invite(self, nickname, channel):
+        return toBytes("INVITE " + nickname + " " + channel)
     
+    def kick(self, channels, users, comment=None):
+        if(comment is None):
+            content = auto_comas(channels, users)
+        else:
+            content = auto_comas(channels, users) + " :" + comment
+        return toBytes("KICK " + content + "\r\n")
     
+    def Privmsg(self, msgtarget, msg):
+        return toBytes("PRIVMSG " + msgtarget + " :" + msg + "\r\n")
+    
+    def Notice(self, msgtarget, txt):
+        return toBytes("NOTICE " + msgtarget + " :" + txt + "\r\n")
+    
+    def Motd(self, target=None):
+        if(target is None):
+            return toBytes("MOTD " + "\r\n")
+        else:
+            return toBytes("MOTD " + target + "\r\n")
